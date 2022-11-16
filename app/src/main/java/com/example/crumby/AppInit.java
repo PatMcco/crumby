171276d6551ab373a150c93c7ba972c7fe9af6ec
PatMcCo <w0183220@nscc.ca>
2022-11-16 14:23:21 -0400

    changed cust info and removed en/fr

--- app/src/main/java/com/example/crumby/AppInit.java
+++ app/src/main/java/com/example/crumby/AppInit.java
@@ -1,15 +1,23 @@
 package com.example.crumby;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.*;
 
 import android.os.Bundle;
 
 public class AppInit extends AppCompatActivity {
 
+    Intent intent = new Intent();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appinit);
+        intent = getIntent();
+
+        if (intent != null) {
+
+        }
+
 
     }
 }
