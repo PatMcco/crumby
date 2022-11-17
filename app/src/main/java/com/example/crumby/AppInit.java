d04561c9f2e54d7a4a15aa4180f5c8b36990686c
PatMcco <w0183220@nscc.ca>
2022-11-17 11:26:43 -0400

    main done, image fixed

--- app/src/main/java/com/example/crumby/AppInit.java
+++ app/src/main/java/com/example/crumby/AppInit.java
@@ -1,23 +1,39 @@
 package com.example.crumby;
-
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.*;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AppInit extends AppCompatActivity {
 
-    Intent intent = new Intent();
+    //buttons
+    private Button startOrder;
+
+
+    private View.OnClickListener startOrderOnClickListener= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startOrderClicked();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appinit);
-        intent = getIntent();
 
-        if (intent != null) {
-
-        }
+        startOrder = new Button(this);
 
 
+        startOrder.setOnClickListener(startOrderOnClickListener);
+
+
+
+    }
+
+    private void startOrderClicked(){
+        Intent intent = new Intent(AppInit.this,NewOrder.class);
+        startActivity(intent);
     }
 }
