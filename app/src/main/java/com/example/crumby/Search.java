e5812b3059c27f25a5f20eb0823e56e1abd416df
PatMcco <w0183220@nscc.ca>
2022-11-18 13:13:26 -0400

    buttons and listeners all declared, ready for code

--- app/src/main/java/com/example/crumby/Search.java
+++ app/src/main/java/com/example/crumby/Search.java
@@ -3,12 +3,39 @@ package com.example.crumby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Search extends AppCompatActivity {
+
+    //buttons
+    Button btn_findAll;
+    Button btn_back2;
+    EditText et_searchResults;
+    EditText et_orderIdSearch;
+
+    private View.OnClickListener findAllListener = v -> findAllClicked();
+    private View.OnClickListener back2Listener = v -> back2Clicked();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        btn_findAll = findViewById(R.id.btn_findAll);
+        btn_back2 = findViewById(R.id.btn_back);
+        et_searchResults = findViewById(R.id.et_searchResults);
+        et_orderIdSearch = findViewById(R.id.et_orderIdSearch);
+        btn_findAll.setOnClickListener(findAllListener);
+        btn_back2.setOnClickListener(back2Listener);
+    }
+
+    private void findAllClicked(){
+        //
+    }
+
+    private void back2Clicked(){
+        //
     }
 }
--- app/src/main/java/com/example/crumby/UpdateOrder.java
+++ app/src/main/java/com/example/crumby/UpdateOrder.java
@@ -3,12 +3,48 @@ package com.example.crumby;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UpdateOrder extends AppCompatActivity {
+
+    //buttons
+    Button btn_find;
+    Button btn_change;
+    Button btn_back;
+    EditText et_orderIdNum;
+
+    //listeners
+    private View.OnClickListener findListener = v -> findClicked();
+    private View.OnClickListener changeListener = v -> changeClicked();
+    private View.OnClickListener backListener = v -> backClicked();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_order);
+
+        btn_find = findViewById(R.id.btn_find);
+        btn_change = findViewById(R.id.btn_change);
+        btn_back = findViewById(R.id.btn_back);
+        et_orderIdNum = findViewById(R.id.orderIdNum);
+        btn_find.setOnClickListener(findListener);
+        btn_change.setOnClickListener(changeListener);
+        btn_back.setOnClickListener(backListener);
     }
+
+    //methods
+    private void findClicked(){
+        //
+    }
+
+    private void changeClicked(){
+        //
+    }
+
+    private void backClicked(){
+        //
+    }
+
 }
